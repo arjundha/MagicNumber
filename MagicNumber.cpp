ea40e0867d376529903612cfbeb5f385c1a1deaf
Arjun <59514930+arjundha@users.noreply.github.com>
2021-05-03 21:03:55 -0700

    First push

---
+++ MagicNumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    // Introductory messages to 
+    std::cout << "You are locked in an evil wizard's tower, and must unlock the many doors to escape.\n";
+    std::cout << "You need to enter the correct codes to continue your escape...\n";
+
+    const int a = 3;
+
+    int b = 7;
+    int c = 1;
+
+    int sum = a + b + c;
+
+    std::cout << sum << std::endl;
+    std::cout << a * b * c << std::endl;
+
+    return 0;
+}
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MagicNumber
 Magic Number is a number guessing game made in C++
+
+init
